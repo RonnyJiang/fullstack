1bd38ae047df3c6904aab286d080c8dacb7de2d7
Ronny <set@aliyun.com>
2016-12-26 02:01:16 +0800

    add one demo

---
+++ P1_035/login.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import getpass
+
+user = input('username:')   #raw_input 在py3中以被取消，不在支持使用
+passwd = input('password:')
+
+print('user name is %s' % user)
+print('password is %s' % passwd)
+
